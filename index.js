067f6c90cd09ebedc225b097e95cd2e4d52beed2
Edwin Brandon Lopez Martinez <k9999-f@hotmail.com>
2021-10-08 11:38:15 -0500

    Cambios realizados

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-let URL = 'https://api.datos.gob.mx/v1/calidadAire';
-
+var URL = 'https://api.datos.gob.mx/v1/calidadAire';
 const API = new XMLHttpRequest();
+let btnDatos = document.getElementById('btnDatos');
 
-API.open('GET', URL, true);
-API.send();
 
-API.onreadystatechange = function() {
-  if(this.status == 200 && this.readyState == 4) {
-    let datos = JSON.parse(this.responseText);
-    let tbody = document.getElementById('tbody');
-
-    console.log(datos.results);
-
-    
-
-    for(let item of datos.results){
-      // for (var i = 0; i < datos.results.length; i++) { console.log(datos[i]); }
-
-      console.log(item.stations);
-      
-      tbody.innerHTML += `<t>
-      <td>${item._id}</td>
-      </t>`;
-    }
-
+function getDate() {
+  API.open('GET', URL, true);
+  API.send();
   
-
-
-
-
+  API.onreadystatechange = function() {
+    if(this.status == 200 && this.readyState == 4) {
+      let datos = JSON.parse(this.responseText);
+      let tbody = document.getElementById('tbody');
+  
+      // console.log(datos.results);
+  
+      for(let item of datos.results){
+  
+        // console.log(item);
+        item.stations.forEach((val, index) => {
+          console.log(val, index);
+  
+          tbody.innerHTML += `<tr>
+          <td>${val.id}</td>
+          <td>${val.location.lat}</td>
+          <td>${val.name}</td>
+          <td>${val.source_id}</td>
+          </tr>`;
+        });
+      }
+    }
   }
 }
+
+
+
+btnDatos.addEventListener('click', function() {
+  getDate();
+});
